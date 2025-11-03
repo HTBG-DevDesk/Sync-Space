ace80bb3fa52b061aaaa6a8040ab43e914d3af30
HTBG Dev Desk. <dev.htbg@gmail.com>
2025-11-03 22:25:38 +0530

    commit
    Commit new project

---
+++ wp-content/themes/freeio/template-jobs/single-employer-elementor.php
@@ -0,0 +1,29 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+
+?>
+
+<section id="primary" class="content-area inner">
+	<div id="main" class="site-main content" role="main">
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+					global $post;
+					do_action( 'freeio_employer_detail_content', $post );
+				// End the loop.
+				endwhile;
+			?>
+		<?php else : ?>
+			<div class="container">
+				<?php get_template_part( 'content', 'none' ); ?>
+			</div>
+		<?php endif; ?>
+	</div><!-- .site-main -->
+</section><!-- .content-area -->
+<?php get_footer(); ?>
